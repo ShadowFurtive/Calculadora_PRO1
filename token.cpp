1f64fc7cdbb690e74f5ae0a4115f9c944bd949ac
ShadowFurtive <silencioso4444@gmail.com>
2020-04-23 18:10:31 +0200

    202005231810_SF_update2.0

--- token.cpp
+++ token.cpp
@@ -1,9 +1,11 @@
 #include "token.hpp"
 #include "string"
+#include <stdio.h>
+#include <string.h>
 //constructors
 
 token::token(){
-	valor="0"
+	valor="0";
 }
 
 token::token(int i){
@@ -12,10 +14,10 @@ token::token(int i){
 		temp=i%10;
 		i= i/10;
 		temp =temp + 48;
-		num = (char)temp + num;
+		valor = (char)temp + valor;
 	}
 	i=i+48;
-	valor = (char)i + num ;
+	valor = (char)i + valor ;
 }
 
 token::token(bool b){
@@ -28,63 +30,113 @@ token::token(string s){
 }
 
 token::token(const char s[]){
-	valor(s);
+	int aux=strlen(s);
+	for (int i = 0; i < aux; i++) { 
+	valor = valor + s[i]; 
+	}  
 }
 
 token::token(const token &t){
 	valor=t.valor;
 }
 
-token::token& operator=(const token &e){
-	valor=e.valor;
-}
-
 token::~token(){
 }
 
-token::bool es_operador_unari() const{
-	if(valor="not") return true;
+token& token:: operator=(const token &e){
+	delete this;
+	string *foo;
+	foo = new string(e.valor);
+	return foo;
+		
+}
+
+bool token::es_operador_unari() const{
+	if(valor=="not") return true;
 	else return false;
 }
 
-token::bool es_operador_binari() const{
-	if(valor="and" or valor="or") return true;
+bool token::es_operador_binari() const{
+	if(valor=="and" or valor=="or") return true;
 	else return false;
 }
 
-token::bool es_operador_commutatiu(){
+bool token::es_operador_commutatiu() const{
 	if(valor=="+" or valor=="*") return true;
 	else return false;
 }
 
-token::bool es_boolea() const{
-	if(valor="T" or valor="F") return true;
+bool token::es_boolea() const{
+	if(valor=="T" or valor=="F") return true;
 	else return false;
 }
-token::bool es_enter() const{
+bool token::es_enter() const{
 	bool trobat = true;
-	for(unsigned int i = 0; i<=numero.size() and trobat; ++i){
-		if(numero[0]== '-') trobat = true;
-			else if (numero[i]<48 and numero[i]>57) trobat = false;
+	for(unsigned int i = 0; i<=valor.size() and trobat; ++i){
+		if(valor[0]== '-') trobat = true;
+			else if (valor[i]<48 and valor[i]>57) trobat = false;
 			else trobat = false;
 	}
 	return trobat;
 }
-token::bool es_variable(){
-	if (not(valor.es_enter() and valor.es_boolea() and valor.es_operador_unari()
-	valor.es_operador_binari() and valor.es_operador_commutatiu() and valor=="%"
-	and valor=="**" and valor=="/" and valor=="==" and valor=="!=" and valor=="-")) return true;
+bool token::es_variable() const{
+	if (not(this->es_enter() and this->es_boolea() and this->es_operador_unari() and this->es_operador_binari() and this->es_operador_commutatiu() and valor=="%" and valor=="**" and valor=="/" and 	valor=="==" and valor=="!=" and valor=="-")) return true;
 	else return false;
 }
-pair<int, bool> prioritat() const;
+
+pair<int, bool> token::prioritat() const{
     // Pre: true
     // Post: Indica el nivell de prioritat a 'first' i si l'associativitat és d'esquerra a dreta a 'second' del p.i.
     //       Considerem que els operands tenen el màxim nivell de prioritat
+	pair<int, bool> res;
+	if (this->es_operador_unari()){
+		res.first=3;
+		res.second=false;
+		return res;
+	}
+	else if(this->es_operador_binari()){
+		if (valor=="and"){
+		res.first=2;
+		res.second=true;
+		return res;
+		}
+		else{
+		res.first=1;
+		res.second=true;
+		return res;
+		}
+	}
+	else if(valor=="==" or valor=="!="){
+		res.first=4;
+		res.second=true;
+		return res;
+	}
+	else if(valor=="+" or valor=="-"){
+		res.first=5;
+		res.second=true;
+		return res;
+	}
+	else if(valor=="*" or valor=="/" or valor=="%"){
+		res.first=6;
+		res.second=true;
+		return res;
+	}
+	else if(valor=="**"){
+		res.first=7;
+		res.second=false;
+		return res;
+	}
+	else{
+		res.first=0;
+		res.second=false;
+		return res;
+	}
+}
 
 
-token::int to_int() const{
+int token::to_int() const{
 	int numero;
-	if(valor.es_enter()){
+	if(this->es_enter()){
 		numero = stoi(valor);
 		return numero;
 	}
@@ -92,32 +144,44 @@ token::int to_int() const{
    
 }
 
-token::bool to_bool() const{
-	if((valor="T" or valor="F") or (valor="1" or valor="0")) return true;
+bool token::to_bool() const{
+	if((valor=="T" or valor=="F") or (valor=="1" or valor=="0")) return true;
 	else return false;
 }
 
-token::string to_string() const{
+string token::to_string() const{
 	return valor;
 }
 
-token::bool operator==(const token &t) const{
+bool token::operator==(const token &t) const{
 // Pre: t = TK
 // Post: Indica si el token del p.i. és igual al token TK
-if((valor==t.valor) return true;
+if(valor==t.valor) return true;
 else return false;
 }
 
-token::bool operator!=(const token &t) const{
+bool token::operator!=(const token &t) const{
 // Pre: t = TK
 // Post: Indica si el token del p.i. és diferent al token TK
 if(valor!=t.valor) return true;
 else return false;
 }
 
-friend istream& operator>>(istream& is, token &t){
+istream& operator>>(istream& is, token &t){
 // Pre: El canal is conté un string amb un contingut d'un token TK
 // Post: t = TK
+	is >> t.valor;
+	return is;
+}
+
+ostream& operator<<(ostream& os, const token &t){
+    // Pre: t = TK
+    // Post: S'ha escrit al canal os el contingut del token TK
+	os << t.valor;
+	return os;
+}
+
+
 	
 
 
